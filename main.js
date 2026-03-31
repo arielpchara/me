3054b253699460b3088111c3609a1ef1267295cf
Ariel Pchara <ariel@pchara.com>
2026-03-30 21:01:08 -0300

    refactor: simplify Google Tag Manager script initialization

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-(function () {
-  var script = document.createElement('script');
-  script.async = true;
-  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-STXPKCW1PC';
-  document.head.appendChild(script);
+var script = document.createElement('script');
+script.async = true;
+script.src = 'https://www.googletagmanager.com/gtag/js?id=G-STXPKCW1PC';
+document.head.appendChild(script);
 
-  window.dataLayer = window.dataLayer || [];
-  function gtag() { dataLayer.push(arguments); }
-  gtag('js', new Date());
-  gtag('config', 'G-STXPKCW1PC');
-})();
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'G-STXPKCW1PC');
